251f07d17db706be73db07e6f61cbba882695b5c
某亚瑟 <mouyase@qq.com>
2019-06-21 19:12:46 +0800

    WiP:Create Project

---
+++ app/src/main/java/moe/yonjigen/kisaragichihaya/MainActivity.java
@@ -0,0 +1,14 @@
+package moe.yonjigen.kisaragichihaya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
