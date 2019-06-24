d724b6a5dd377abf11112fbc0522572af99700a3
某亚瑟 <mouyase@qq.com>
2019-06-25 00:24:02 +0800

    WiP:Update

--- app/src/main/java/moe/yonjigen/ui/utils/KisaragiChihaya.java
+++ app/src/main/java/moe/yonjigen/ui/utils/KisaragiChihaya.java
@@ -5,7 +5,21 @@ import android.webkit.WebView;
 public class KisaragiChihaya {
     WebView mWebView;
 
-    KisaragiChihaya() {
+    private KisaragiChihaya() {
+    }
 
+    public void setWebView(WebView webView) {
+        this.mWebView = webView;
+    }
+
+    public void addFlat(String key, Flat flat) {
+
+    }
+
+    public void runFromJS() {
+    }
+
+    public interface Flat {
+        void run(String json);
     }
 }
